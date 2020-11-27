5775bfbf93b37fc33cf905e5561ee918c3f97307
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2020-11-28 00:23:13 +0530

    Update Slingshot.js

--- Slingshot.js
+++ Slingshot.js
@@ -16,12 +16,12 @@ class SlingShot{
     }
 
     display(){
-        if(this.Slingshot.bodyA){
+        
             var pointA = this.Slingshot.bodyA.position;
             var pointB = this.pointB;
             strokeWeight(4);
             line(pointA.x, pointA.y, pointB.x, pointB.y);
-        }
+        
     }
     
 }
